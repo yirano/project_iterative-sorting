2616bbb3f5937e66bb8c292e0fc6001d4067927d
yirano <yirano@protonmail.ch>
2020-09-28 09:57:30 -0400

    Completed searching lecture

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,4 @@
 def linear_search(arr, target):
-    # Your code here
     for i in range(0, len(arr)):
         if arr[i] == target:
             return i
@@ -9,11 +8,21 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    # get the middle point
+    # compare the value in the middle with target
+    start = 0
+    end = (len(arr)-1)
 
-    # Your code here
-    arr = sorted(arr)
-    center_ind = len(arr) // 2
-    if arr[center_ind] =< target:
-        
+    found = False
+    while end >= start and not found:
+        middle_ind = (start + end) // 2
+        if arr[middle_ind] == target:
+            found = True
+            return middle_ind
+        else:
+            if target < arr[middle_ind]:
+                end = middle_ind - 1
+            if target > arr[middle_ind]:
+                start = middle_ind + 1
 
-    return arr  # not found
+    return -1
